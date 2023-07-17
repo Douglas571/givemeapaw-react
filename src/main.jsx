7c02e9461f7ff34fdaba5763e8ccd6c2e10e707a
Douglas Socorro <douglassocorro.work@gmail.com>
2023-07-16 22:15:17 -0400

    Adde a testing store

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 
+
+
 // Tirth party CSS
 import 'material-icons/iconfont/material-icons.css'
 
 // Own CSS
 import './index.css'
 
+import { Provider, useSelector, useDispatch } from 'react-redux'
+
+// import { increment, decrement } from './feactures/counter'
+import store from './store'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 )
