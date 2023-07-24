bd507543039b7939183742dfbefd42b784a56bc8
Douglas Socorro <douglassocorro.work@gmail.com>
2023-07-24 17:14:41 -0400

    Make some changes and added a TODO file

---
+++ TODO.md
@@ -0,0 +1,48 @@
+# List of things to complete
+
+## Refactorings
+
+- make a layout component for basic usage
+
+## PAGES
+
+## home
+
+- center the jumbotron title
+- remove old styled code
+
+### /me page
+
+- [x] fix layout in the top
+- [] inprove the campañas preview section
+- [] add a footer
+- [] add functin to request voluntary verified
+
+### /user/campaigns
+
+- Add a menu button in the app bar, with functions:
+
+  - create, edit, delete.
+
+- fix iu: make the searchbar take full width
+
+### /user/adoptions
+
+- Add function to publish adoptions:
+
+  - define the data structure
+  - add function to backend
+  - designe interface
+  - add function to frontend
+
+### /campaigns
+
+- make responsive
+- Add function to search campaigns
+- add footer
+
+## COMPONENTS
+
+### SideMenu
+
+- add gaps to sidebar
--- src/views/User.jsx
+++ src/views/User.jsx
@@ -13,11 +13,10 @@ import { api as API } from '@/libs'
 import KAppBar from '@/components/KAppBar';
 import ProgressBar from '../components/ProgressBar'
 import Link from '@/components/Link'
-import Box from '@/components/Box'
 import Title from '@/components/Title'
 
 import DashboardCampaignShorcut from '../components/DashboardCampaignShorcut'
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, Box, Paper, Stack } from '@mui/material'
 
 
 const APP_TITLE = "Danos Una Pata"
@@ -30,64 +29,63 @@ const User = () => {
   const [ campaigns, updateCampaigns ] = useCampaigns()
 
   return (
-    <div >
+    <>
       <KAppBar/>
-      <div className='p-5'>
-
-        {/* Greeting box */}
-        <Box>
-          <div className='flex justify-center align-center'>
-            <Typography variant='h5'>Bienvenido {user.username}</Typography>
-            <span className='text-7xl'>👋</span>
-          </div>
-        </Box>
-
-        <Box className="info box">
-          <Typography variant='body1'>En {APP_TITLE} nos aseguramos de que quienes soliciten 
-          ayuda sean personas honestas, con intenciones reales
-          de ayudar a estos adorables seres que día a día sufren
-          en las calles. Si deceas ser parte de esta gran familia te pedimos
-          amablemente que hables con nosotros antes de solicitar ayuda.</Typography>
-          <Button>Solicitar Rol de Voluntario</Button>
-        </Box>
-
-        {/* Campaigns Shorcut List */}
-        <Box className='box'>
-          <div className='mb-5'>
-            <Typography variant='h5'>Campañas</Typography>
-          </div>
-          <div>
+      <Box sx={{ mt: 7, p: 3, minHeight: '100vh' }}>
+        <Stack gap={2}>
+          
+          {/* Greeting box */}
+          <Paper sx={{ p: 2 }}>
             
-            { campaigns.map( c => (
-                <DashboardCampaignShorcut campaign={c} /> 
-            ))}
-          </div>
-          <div className='text-center'>
-            <Link to='/me/campaigns'>Administrar Campañas</Link>
-          </div>
-        </Box>
+          <Typography variant='h5' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            Bienvenido {user.username}
+            <span className='text-7xl'>👋</span>
+          </Typography>
+              
+            
+          </Paper>
 
-        {/* Donations Shorcut List */}
-        {/* <Box>
-          <Title>Donaciones Pendientes</Title>
-          <div>
-            <div className='donation-item bordered-box hover:shadow-lg'>
-              <img className='avatar'/>
-              <div>
-                <h3 className='title'>Juán Gabriel</h3>
-                <p>para: Goldens</p>
-                <h6 className='text-primary font-bold text-5xl'>100,00$</h6>
+          <Paper sx={{ p: 2 }}>
+            <Typography variant='body1'>En {APP_TITLE} nos aseguramos de que quienes soliciten 
+            ayuda sean personas honestas, con intenciones reales
+            de ayudar a estos adorables seres que día a día sufren
+            en las calles. Si deceas ser parte de esta gran familia te pedimos
+            amablemente que hables con nosotros antes de solicitar ayuda.</Typography>
+            <Button>Solicitar Rol de Voluntario</Button>
+          </Paper>
+
+          {/* Campaigns Shorcut List */}
+          <Paper sx={{ p: 2 }}>
+            <Typography variant='h5' sx={{ mb: 2}}>Campañas</Typography>
+            <Box>
+              { campaigns.map( c => (
+                  <DashboardCampaignShorcut campaign={c} /> 
+              ))}
+            </Box>
+            <Link to='/me/campaigns'>Administrar Campañas</Link>
+          </Paper>
+
+          {/* Donations Shorcut List */}
+          {/* <Box>
+            <Title>Donaciones Pendientes</Title>
+            <div>
+              <div className='donation-item bordered-box hover:shadow-lg'>
+                <img className='avatar'/>
+                <div>
+                  <h3 className='title'>Juán Gabriel</h3>
+                  <p>para: Goldens</p>
+                  <h6 className='text-primary font-bold text-5xl'>100,00$</h6>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='text-center mt-5'>
-            <Link to='/me/donations'>Ver Más</Link>
-          </div>
-        </Box> */}
+            <div className='text-center mt-5'>
+              <Link to='/me/donations'>Ver Más</Link>
+            </div>
+          </Box> */}
 
-        <Outlet/>
-      </div>
-    </div>
+        </Stack>
+      </Box>
+    </>
   )
 }
 
